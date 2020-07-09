fb21303ea216ca363b6650b6e34fa93311afce9d
Agatha Heterodyne <sterling.dorminey@gmail.com>
2020-07-08 23:37:51 -0700

    Cool, seems to work.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
+  resolve: {
+    extensions: [ '.ts', '.js' ]
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
