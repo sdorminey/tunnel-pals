641012c26ce90fca549f5de5fd803e1b40663107
Agatha Heterodyne <sterling.dorminey@gmail.com>
2020-07-08 23:49:32 -0700

    Fixed TS loading!

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,8 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+const c = document.getElementById('canvas') as HTMLCanvasElement;
+const ctx = c.getContext('2d');
+ctx.fillStyle = "#ff0000";
+ctx.fillRect(0, 0, 320, 240);
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,15 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
   resolve: {
     extensions: [ '.ts', '.js' ]
   },
